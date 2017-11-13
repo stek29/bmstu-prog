c21f687b8a2466b192e8cef91455946a4ca491c3
Viktor Oreshkin <imselfish@stek29.rocks>
2017-11-14 01:16:51 +0300

    lr7p1: Add more user-interaction

--- Sem3/lr7/p1/user.rb
+++ Sem3/lr7/p1/user.rb
@@ -3,6 +3,18 @@ require_relative 'src'
 H_FILENAME = 'h.txt'.freeze
 F_FILENAME = 'f.txt'.freeze
 
+printf 'Create file F? [yN] '
+if gets.chomp.casecmp('y') == 0
+  puts 'Enter file F contents'
+  File.open(F_FILENAME, 'w') do |f_file|
+    loop do
+      line = gets.to_s.chomp
+      break if line.empty?
+      f_file.puts line
+    end
+  end
+end
+
 h_file = File.open(H_FILENAME, 'w')
 
 File.new(F_FILENAME).each_line do |line|
@@ -10,3 +22,9 @@ File.new(F_FILENAME).each_line do |line|
 end
 
 h_file.close
+
+puts "\nFile F:"
+File.new(F_FILENAME).each_line { |l| puts l }
+
+puts "\nFile H:"
+File.new(H_FILENAME).each_line { |l| puts l }
