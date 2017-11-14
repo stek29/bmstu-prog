840c7127c8536fd3b104e9c54f8395da000d31ec
Viktor Oreshkin <imselfish@stek29.rocks>
2017-11-14 11:19:23 +0300

    lr8 : Use only @m_i and @n_i
    Also fix a typo :D

--- Sem3/lr8_app/app/controllers/bin_reverser_controller.rb
+++ Sem3/lr8_app/app/controllers/bin_reverser_controller.rb
@@ -1,15 +1,12 @@
 class BinReverserController < ApplicationController
   def input
-    # nothing interesting in inputg
+    # nothing interesting in input
   end
 
   def view
     redirect_to bin_reverser_input_url unless params[:m]
 
     @m_i = params[:m].to_i
-
-    @m_bin_str = @m_i.to_s 2
-    @n_bin_str = @m_bin_str.reverse
-    @n_i = @n_bin_str.to_i 2
+    @n_i = @m_i.to_s(2).reverse.to_i(2)
   end
 end
