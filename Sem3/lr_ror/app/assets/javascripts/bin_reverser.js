a3ba6144bd3881f65c4740a95ec9cd616255769f
Viktor Oreshkin <imselfish@stek29.rocks>
2017-11-27 23:08:47 +0300

    yay ajax

--- Sem3/lr_ror/app/assets/javascripts/bin_reverser.js
+++ Sem3/lr_ror/app/assets/javascripts/bin_reverser.js
@@ -1,1 +1,44 @@
-console.log('FUCK COFFEE SCRIPT');
+// = require jquery3
+
+$(document).ready(function() {
+    var result_div = $('#result');
+    result_div.show();
+
+    var render_to = {
+        'm_int': result_div.find('#m'),
+        'm_bin': result_div.find('#m_bin'),
+        'n_int': result_div.find('#n'),
+        'n_bin': result_div.find('#n_bin')
+    };
+
+    var form = $('#m_form');
+    var status = $('#status');
+    var hide_these = $('.hide-on-fail');
+
+    hide_these.hide();
+    status.text('Enter Number and press Submit');
+
+    form.on('ajax:send', function() {
+        status.text('Loading');
+        hide_these.hide();
+    });
+
+    form.on('ajax:success', function(event) {
+        console.log(event);
+
+        var data = event.detail[0];
+
+        render_to.m_int.text(data.num_m.int);
+        render_to.m_bin.text(data.num_m.bin_str);
+        render_to.n_int.text(data.num_n.int);
+        render_to.n_bin.text(data.num_n.bin_str);
+
+        status.text('Result:');
+        hide_these.show();
+    });
+
+    form.on('ajax:error', function() {
+        console.log(event);
+        status.text('Error!');
+    })
+});
--- Sem3/lr_ror/app/controllers/bin_reverser_controller.rb
+++ Sem3/lr_ror/app/controllers/bin_reverser_controller.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+
 class BinReverserController < ApplicationController
   def input
     # nothing interesting in input
@@ -5,16 +7,23 @@ class BinReverserController < ApplicationController
 
   def view
     m_int = params[:m].to_i
-    redirect_to bin_reverser_input_url if m_int.nil?
-    m_bin = m_int.to_s 2
-    n_bin = m_bin.reverse
+    if m_int.nil? || m_int.negative?
+      if request.xhr?
+        render body: nil, status: 400
+      else
+        redirect_to bin_reverser_input_url
+      end
+    else
+      m_bin = m_int.to_s 2
+      n_bin = m_bin.reverse
 
-    @num_m = { int: m_int, bin_str: m_bin }
-    @num_n = { int: n_bin.to_i(2), bin_str: n_bin }
+      @num_m = { int: m_int, bin_str: m_bin }
+      @num_n = { int: n_bin.to_i(2), bin_str: n_bin }
 
-    respond_to do |format|
-      format.html
-      format.json { render json: { num_m: @num_m, num_n: @num_n } }
+      respond_to do |format|
+        format.html
+        format.json { render json: { num_m: @num_m, num_n: @num_n } }
+      end
     end
   end
 end
