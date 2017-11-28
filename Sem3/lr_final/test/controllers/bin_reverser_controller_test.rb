869af926eff14590657be4f18f2539d6bc2fbfd8
Viktor Oreshkin <imselfish@stek29.rocks>
2017-11-28 10:51:15 +0300

    [lr_final] ReverseNumber model tests

--- Sem3/lr_final/test/controllers/bin_reverser_controller_test.rb
+++ Sem3/lr_final/test/controllers/bin_reverser_controller_test.rb
@@ -5,10 +5,4 @@ class BinReverserControllerTest < ActionDispatch::IntegrationTest
     get bin_reverser_input_url
     assert_response :success
   end
-
-  test "should get view" do
-    get bin_reverser_view_url
-    assert_response :success
-  end
-
 end
--- Sem3/lr_final/test/models/reverse_number_test.rb
+++ Sem3/lr_final/test/models/reverse_number_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ReverseNumberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not allow duplicates' do
+    orig = ReverseNumber.create(m_int: 15)
+
+    assert_raises ActiveRecord::RecordNotUnique do
+      ReverseNumber.create(m_int: 15)
+    end
+  end
+
+  test 'should find after creation' do
+    created = ReverseNumber.find_or_create_by(m_int: 42)
+    found = ReverseNumber.find_by(m_int: 42)
+
+    assert_not found.nil?
+    assert_equal created.n_int, found.n_int
+  end
+
+  test 'does not allow invalid m' do
+    assert_not ReverseNumber.create(m_int: -33).valid?
+  end
+
+  test 'gives correct results' do
+    x90 = ReverseNumber.find_or_create_by(m_int: 90)
+    assert_equal x90.n_int, 45
+  end
 end
