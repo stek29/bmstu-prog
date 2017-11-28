b831d73c7195cf971c69006911620ab8463a2b35
Viktor Oreshkin <imselfish@stek29.rocks>
2017-11-28 05:59:40 +0300

    [lr_final] Move logic into Model and Helper

--- Sem3/lr_final/app/controllers/bin_reverser_controller.rb
+++ Sem3/lr_final/app/controllers/bin_reverser_controller.rb
@@ -6,27 +6,18 @@ class BinReverserController < ApplicationController
   def input; end
 
   def view
-    @result = ReverseNumber.new(
-      m_int: @m_int,
-      m_bin: @m_int.to_s(2),
-      n_bin: @m_int.to_s(2).reverse.to_i(2).to_s(2),
-      n_int: @m_int.to_s(2).reverse.to_i(2)
-    )
+    @result = ReverseNumber.new(make_n_m(@m_int))
+
+    unless @result.valid?
+      flash[:errors] = @result.errors.messages
+      redirect_to bin_reverser_input_url
+    end
   end
 
   private
 
   def parse_params
     @m_int = to_i_or_nil(params[:m])
-    error = if @m_int.nil?
-               'M is not Integer'
-             elsif !@m_int.positive?
-               'M should be greater than zero'
-             end
-    unless error.nil?
-      flash[:danger] = error
-      redirect_to bin_reverser_input_url
-    end
   end
 
 end
--- Sem3/lr_final/app/models/reverse_number.rb
+++ Sem3/lr_final/app/models/reverse_number.rb
@@ -1,2 +1,13 @@
 class ReverseNumber < ApplicationRecord
+  validate :m_int_valid
+
+  private
+
+  def m_int_valid
+    if self.m_int.nil?
+      errors.add :m_int, 'cannot be nil'
+    elsif !self.m_int.positive?
+      errors.add :m_int, 'must be positive'
+    end
+  end
 end
